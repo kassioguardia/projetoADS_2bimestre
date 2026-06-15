b6948474bcaf2c703572fce9900a5e956448de5e
kassioguardia <kassioguardianot@gmail.com>
2026-06-14 21:01:29 -0300

    sprint2,2

--- templates/header.php
+++ templates/header.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <title>Hoffmann Lab 3D</title>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-
 <header>
 <nav class="navbar navbar-expand-lg navbar-dark bg-black fixed-top py-3" style="margin-top:0;">
     <div class="container-fluid px-4">
