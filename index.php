1c62ebeb78f3ce2ebefc8058dd76c7756b914f78
kassioguardia <kassioguardianot@gmail.com>
2026-06-14 21:01:57 -0300

    sprint2.1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="icon" type="image/png" sizes="48x48" href="img/logo complexa com fundo.png">
     <title>Hoffmann Lab 3D</title>
 </head>
